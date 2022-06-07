f846efa0ae3c1e17af4453d5a9280c048ee65cad
Colin Steele <colin.steele20@gmail.com>
2022-06-07 10:03:30 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import "react-piano/dist/styles.css";
+
+function App() {
+  const firstNote = MidiNumbers.fromNote("c3");
+  const lastNote = MidiNumbers.fromNote("f5");
+  const keyboardShortcuts = KeyboardShortcuts.create({
+    firstNote: firstNote,
+    lastNote: lastNote,
+    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+  });
+
+  return (
+    <Piano
+      noteRange={{ first: firstNote, last: lastNote }}
+      playNote={(midiNumber) => {
+        // Play a given note - see notes below
+      }}
+      stopNote={(midiNumber) => {
+        // Stop playing a given note - see notes below
+      }}
+      width={1000}
+      keyboardShortcuts={keyboardShortcuts}
+    />
+  );
+}
+
+export default App;
