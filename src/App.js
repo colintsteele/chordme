ab7e385f8024960fe7bb51214b75e0c5d1f4e021
Colin Steele <colin.steele20@gmail.com>
2022-06-07 10:48:33 -0400

    beginning to add my stuff

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
 import "react-piano/dist/styles.css";
+import "./Theory";
 
 function App() {
-  const firstNote = MidiNumbers.fromNote("c3");
-  const lastNote = MidiNumbers.fromNote("f5");
+  const firstNote = MidiNumbers.fromNote("c3"); //48
+  const lastNote = MidiNumbers.fromNote("b4"); //71
+  const notes = {};
   const keyboardShortcuts = KeyboardShortcuts.create({
     firstNote: firstNote,
     lastNote: lastNote,
     keyboardConfig: KeyboardShortcuts.HOME_ROW,
   });
+  //console.log(sample(twoOctave)); sample might work in es6
 
   return (
     <Piano
+      activeNotes={[48, 71]}
       noteRange={{ first: firstNote, last: lastNote }}
       playNote={(midiNumber) => {
         // Play a given note - see notes below
